e399d4d12bedf671a25616bc4194c2c2c823197c
Aaron Kolker <akolker3@gmail.com>
2015-06-17 16:31:22 +0100

    made a function to draw squares. This does not work so fffffffuuuuuuuuuuuu

--- game.js
+++ game.js
@@ -2,6 +2,16 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 //draw the background
+var background = ctx;
+background.rect(0,0,640,480);
+background.fill();
 
-ctx.rect(0,0,640,480);
-ctx.fill();
+//draw some shiteee
+var drawSquare = function (size, posX, posY) {
+	var rect = ctx;
+	rect.fillStyle = "white";
+	rect.fillRect(posX,posY, posX + size,posY + size);
+}
+
+drawSquare(10,25,25);
+drawSquare(10,50,100);
