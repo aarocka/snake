087ad8bbf2eec3a5eaeb27c7a06274a3670596ac
Aaron Kolker <akolker3@gmail.com>
2015-07-30 20:50:16 -0400

    this fucking piece of shit does something at least

--- game.js
+++ game.js
@@ -4,23 +4,18 @@ var ctx = canvas.getContext("2d");
 //draws a black background
 
 //draws a black background
+//Ok wtf this function works
+
 function drawBackground(){
 	ctx.fillStyle = '#000'
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 };
 
-//Ok wtf this function works
-/*
-function drawBackground(){
-	ctx.fillStyle = '#000'
-	ctx.fillRect(0,0,canvas.width,canvas.height);
-};
-*/
 //yet this one doesn't
 /*
 function drawBackground(){
 	ctx.rect(0,0,canvas.width,canvas.height);
-	ctx.fillStyle = '#FFF'
+	ctx.fillStyle = '#000'
 	ctx.fill();
 };
 */
@@ -33,7 +28,7 @@ var player = {
 	vx: 5,
 	vy: 2,
 	size:10,
-	color:'blue',
+	color:'white',
 	draw : function(){
 		ctx.rect(this.x, this.y, this.size, this.size);
 		ctx.fillStyle = this.color;
@@ -43,10 +38,18 @@ var player = {
 
 function draw(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
+	drawBackground();
 	player.draw();
 	player.x += player.vx;
 	player.y += player.vy;
-	window.requestAnimationFrame(draw);
+	if (player.y + player.vy > canvas.height || player.y + player.vy < 0) {
+  		player.vy = -player.vy;
+	}
+	if (player.x + player.vx > canvas.width || player.x + player.vx < 0) {
+  		player.vx = -player.vx;
+	}
+	//window.requestAnimationFrame(draw);
+	setTimeout()
 };
-drawBackground();
-player.draw();
+
+draw();
