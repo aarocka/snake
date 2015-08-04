55366e13ccd01368d2aa4237fa5a85bec5275c0a
Aaron Kolker <akolker3@gmail.com>
2015-08-04 12:02:24 -0400

    updated gulp file default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@ var gulp = require('gulp'),
 	qunit = require('gulp-qunit'),
 	bower = require('gulp-bower');
 	
-gulp.task('bower', function(){
-	return bower()
-	.pipe(gulp.dest('lib/'));
-});
+
 gulp.task('default', function(){
+	return bower()
+	.pipe(gulp.dest('bower_components/'));
 	return gulp.src('./test/test.html')
 	.pipe(qunit());
 });
