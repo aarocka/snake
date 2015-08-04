5c50443b68ad1f2e648f64843276e8302a7b4754
Aaron Kolker <akolker3@gmail.com>
2015-08-04 12:18:57 -0400

    changed gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
 	qunit = require('gulp-qunit'),
 	bower = require('gulp-bower');
-	
-
-gulp.task('default', function(){
-	return bower()
-	.pipe(gulp.dest('bower_components/'));
+gulp.task('test', function(){
 	return gulp.src('./test/test.html')
 	.pipe(qunit());
 });
+gulp.task('bower', function(){
+	return bower()
+	.pipe(gulp.dest('bower_components/'));
+})
