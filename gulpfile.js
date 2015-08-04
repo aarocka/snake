aea8ecd52dbed262997b6feab7acc8fbc28865fc
Aaron Kolker <akolker3@gmail.com>
2015-08-04 11:57:26 -0400

    fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,11 @@ var gulp = require('gulp'),
 	qunit = require('gulp-qunit'),
 	bower = require('gulp-bower');
 	
+gulp.task('bower', function(){
+	return bower()
+	.pipe(gulp.dest('lib/'));
+});
 gulp.task('default', function(){
 	return gulp.src('./test/test.html')
 	.pipe(qunit());
-});
-
-gulp.task(bower, function(){
-	return bower()
-	.pipe(gulp.dest('lib/'));
 });
