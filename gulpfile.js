390434bc030df43f4d065ffeabd4a1fe6b4603cc
Aaron Kolker <akolker3@gmail.com>
2015-08-04 11:38:29 -0400

    added bower task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
-	qunit = require('gulp-qunit');
+	qunit = require('gulp-qunit'),
+	bower = require('gulp-bower');
 	
 gulp.task('default', function(){
 	return gulp.src('./test/test.html')
 	.pipe(qunit());
+});
+
+gulp.task(bower, function(){
+	return bower()
+	.pipe(gulp.dest('lib/'));
 });
