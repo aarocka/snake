575e063d3f41fe81fd38322ba31b5c5ac0c0ede9
Aaron Kolker <akolker3@gmail.com>
2015-08-05 14:35:34 -0400

    Fixed the square and added broken keyboard controlls

--- game.js
+++ game.js
@@ -2,40 +2,27 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 //draws a black background
-
 //draws a black background
 //Ok wtf this function works
-function consoleJunk (){
-	console.log("Hello there. It seems you've stumbled upon the JavaScript console.");
-	console.log("This is awesome because if you type help() you can see a list of commands");
-}
+
 function drawBackground(){
 	ctx.fillStyle = '#000';
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 }
-
-//yet this one doesn't
-/*
-function drawBackground(){
-	ctx.rect(0,0,canvas.width,canvas.height);
-	ctx.fillStyle = '#000'
-	ctx.fill();
-};
-*/
-//JS Y U NO WORK!!!
-
 //set's up the player
 var player = {
-	x:0,
-	y:0,
-	vx: 5,
-	vy: 2,
+	x:100,
+	y:100,
+	vx: 0,
+	vy: 0,
 	size:10,
 	color:'white',
 	draw : function(){
+		ctx.beginPath();
 		ctx.rect(this.x, this.y, this.size, this.size);
 		ctx.fillStyle = this.color;
 		ctx.fill();
+		ctx.closePath();
 	},
 	colision : function(){
 		if (player.y + player.vy > canvas.height || player.y + player.vy < 0) {
@@ -49,15 +36,70 @@ var player = {
 		player.x += player.vx;
 		player.y += player.vy;
 	}
-}
+};
 
 function draw(){
-	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.clearRect(0,0,canvas.width, canvas.height);
 	drawBackground();
 	player.draw();
 	player.move();
 	player.colision();
 	window.requestAnimationFrame(draw);
-};
+}
 
-draw();
+/* char codes
+87 = w
+83 = s
+65 = a
+68 = d
+38 = up
+40 = down
+37 = left
+39 = right
+*/
+window.addEventListener('keydown', function(e){
+	//console.log(e.keyCode);
+	
+	if (e.keyCode == 83) {
+		console.log('player pressed s');
+		player.vx=0;
+		player.vy=5;
+		draw();
+	}
+	if (e.keyCode == 87) {
+		console.log('player pressed w');
+		player.vx = 0;
+		player.vy = -5;
+		draw();
+	}
+	if (e.keyCode == 65){
+		console.log('player pressed a');
+		player.vx = -5;
+		player.vy = 0;
+		draw();
+	}
+	if (e.keyCode == 68) {
+		console.log('player pressed d');
+		player.vx = 5;
+		player.vy = 0;
+		draw();	
+	}
+});
+window.addEventListener('keyup', function(e){
+	if (e.keyCode == 83) {
+		player.vx = 0;
+		player.vy = 0;
+	}
+	if (e.keyCode == 87) {
+		player.vx = 0;
+		player.vy = 0;
+	}
+	if (e.keyCode == 65){
+		player.vx = 0;
+		player.vy = 0;
+	}
+	if (e.keyCode == 68) {
+		player.vx = 0;
+		player.vy = 0;
+	}
+});
