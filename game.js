bb6e5f8604c7e8e3443ccb4be5b7335ed99680ff
Aaron Kolker <akolker3@gmail.com>
2015-08-09 00:13:46 -0400

    fuck yo shit

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ var player = {
 	vy: 0,
 	size:10,
 	color:'white',
+	//This draws the player square
 	draw : function(){
 		ctx.beginPath();
 		ctx.rect(this.x, this.y, this.size, this.size);
@@ -24,6 +25,7 @@ var player = {
 		ctx.fill();
 		ctx.closePath();
 	},
+	//This handles the colision with the walls if forced into easy mode
 	colision : function(){
 		if (player.y + player.vy > canvas.height || player.y + player.vy < 0) {
   			player.vy = -player.vy;
@@ -32,9 +34,75 @@ var player = {
   			player.vx = -player.vx;
 		}
 	},
+	//This moves the player
 	move : function(){
 		player.x += player.vx;
 		player.y += player.vy;
+	},
+	//This is an input handler or some shit I guess
+	input : function(){
+		//Deals with keydown
+		window.addEventListener('keydown', function(e){
+			if (e.keyCode === 83) {
+				console.log('player pressed s');
+				player.vx=0;
+				player.vy=5;
+				draw();
+			}
+			if (e.keyCode === 87) {
+				console.log('player pressed w');
+				player.vx = 0;
+				player.vy = -5;
+				draw();
+			}
+			if (e.keyCode === 65){
+				console.log('player pressed a');
+				player.vx = -5;
+				player.vy = 0;
+				draw();
+			}
+			if (e.keyCode === 68) {
+				console.log('player pressed d');
+				player.vx = 5;
+				player.vy = 0;
+				draw();	
+			}
+		});
+		//Deals with key up events
+		window.addEventListener('keyup', function(e){
+			if (e.keyCode === 83) {
+				player.vx = 0;
+				player.vy = 0;
+			}
+			if (e.keyCode === 87) {
+				player.vx = 0;
+				player.vy = 0;
+			}
+			if (e.keyCode === 65){
+				player.vx = 0;
+				player.vy = 0;
+			}
+			if (e.keyCode === 68) {
+				player.vx = 0;
+				player.vy = 0;
+			}
+		});
+	},
+	//The following functions are called by the input handler to move the player left and right
+	move_left: function(){
+		
+	},
+	move_right: function(){
+		
+	},
+	move_up: function(){
+		
+	},
+	move_down: function(){
+		
+	},
+	//handles the player eating a square or something
+	eat: function () {
 	}
 };
 
@@ -57,49 +125,4 @@ function draw(){
 37 = left
 39 = right
 */
-window.addEventListener('keydown', function(e){
-	//console.log(e.keyCode);
-	
-	if (e.keyCode === 83) {
-		console.log('player pressed s');
-		player.vx=0;
-		player.vy=5;
-		draw();
-	}
-	if (e.keyCode === 87) {
-		console.log('player pressed w');
-		player.vx = 0;
-		player.vy = -5;
-		draw();
-	}
-	if (e.keyCode === 65){
-		console.log('player pressed a');
-		player.vx = -5;
-		player.vy = 0;
-		draw();
-	}
-	if (e.keyCode === 68) {
-		console.log('player pressed d');
-		player.vx = 5;
-		player.vy = 0;
-		draw();	
-	}
-});
-window.addEventListener('keyup', function(e){
-	if (e.keyCode === 83) {
-		player.vx = 0;
-		player.vy = 0;
-	}
-	if (e.keyCode === 87) {
-		player.vx = 0;
-		player.vy = 0;
-	}
-	if (e.keyCode === 65){
-		player.vx = 0;
-		player.vy = 0;
-	}
-	if (e.keyCode === 68) {
-		player.vx = 0;
-		player.vy = 0;
-	}
-});
+
