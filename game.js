35a8159bca659896e7e96dfe989fbb689ccf415a
Aaron Kolker <akolker3@gmail.com>
2015-09-03 11:58:02 -0400

    Don't worry babby!!!

--- game.js
+++ game.js
@@ -6,109 +6,109 @@ var ctx = canvas.getContext("2d");
 //Ok wtf this function works
 
 function drawBackground() {
-  ctx.fillStyle = '#000';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 //set's up the player
 var player = {
-  x: 200,
-  y: 200,
-  vx: 0, //current velocity in x and y direction
-  vy: 0,
-  speed: 5, //This is the player speed and it determines the vx and vy
-  size: 10,
-  color: 'white',
-  //This draws the player square
-  draw: function() {
-    ctx.beginPath();
-    ctx.rect(this.x, this.y, this.size, this.size);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-  },
-  //This handles the colision with the walls if forced into easy mode
-  colision: function() {
-    if (player.y + player.vy > canvas.height || player.y + player.vy < 0) {
-      this.vy = -this.vy;
-    }
-    if (player.x + player.vx > canvas.width || player.x + player.vx < 0) {
-      this.vx = -this.vx;
-    }
-  },
-  //This is what actually moves the player according to the current velocity. This function is now depreciated and is scheduled for deletion i guess
-  move: function() {
-    player.x += player.vx;
-    player.y += player.vy;
-  },
-  //This is an input handler or some shit I guess
-  input: function() {
-    //Deals with keydown
-    window.addEventListener('keydown', function(e) {
-      e.repeat
-      if (e.keyCode === 83) {
-        console.log('player pressed s');
-        player.move_down();
-      }
-      if (e.keyCode === 87) {
-        console.log('player pressed w');
-        player.move_up();
-      }
-      if (e.keyCode === 65) {
-        console.log('player pressed a');
-        player.move_left();
-      }
-      if (e.keyCode === 68) {
-        console.log('player pressed d');
-        player.move_right();
-      }
-    });
-    //Deals with key up events
-    window.addEventListener('keyup', function(e) {
-      if (e.keyCode === 83) {
-        player.vx = 0;
-        player.vy = 0;
-      }
-      if (e.keyCode === 87) {}
-      if (e.keyCode === 65) {}
-      if (e.keyCode === 68) {}
-    });
-  },
-  //The following functions are called by the input handler to move the player left and right
-  move_left: function() {
-    this.vx = -this.speed;
-    this.vy = 0;
-    this.x += this.vx;
-    this.y += this.vy;
-  },
-  move_right: function() {
-    this.vx = this.speed;
-    this.vy = 0;
-    this.x += this.vx;
-    this.y += this.vy;
-  },
-  move_up: function() {
-    this.vx = 0;
-    this.vy = -this.speed;
-    this.x += this.vx;
-    this.y += this.vy;
-  },
-  move_down: function() {
-    this.vx = 0;
-    this.vy = this.speed;
-    this.x += this.vx;
-    this.y += this.vy;
-  },
-  //handles the player eating a square or something
-  eat: function() {}
+    x: 200,
+    y: 200,
+    vx: 0, //current velocity in x and y direction
+    vy: 0,
+    speed: 5, //This is the player speed and it determines the vx and vy
+    size: 10,
+    color: 'white',
+    //This draws the player square
+    draw: function() {
+        ctx.beginPath();
+        // ctx.rect(this.x, this.y, this.size, this.size);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    },
+    //This handles the colision with the walls if forced into easy mode
+    colision: function() {
+        if (player.y + player.vy > canvas.height || player.y + player.vy < 0) {
+            this.vy = -this.vy;
+        }
+        if (player.x + player.vx > canvas.width || player.x + player.vx < 0) {
+            this.vx = -this.vx;
+        }
+    },
+    //This is what actually moves the player according to the current velocity. This function is now depreciated and is scheduled for deletion i guess
+    move: function() {
+        player.x += player.vx;
+        player.y += player.vy;
+    },
+    //This is an input handler or some shit I guess
+    input: function() {
+        //Deals with keydown
+        window.addEventListener('keydown', function(e) {
+            e.repeat
+            if (e.keyCode === 83) {
+                console.log('player pressed s');
+                player.move_down();
+            }
+            if (e.keyCode === 87) {
+                console.log('player pressed w');
+                player.move_up();
+            }
+            if (e.keyCode === 65) {
+                console.log('player pressed a');
+                player.move_left();
+            }
+            if (e.keyCode === 68) {
+                console.log('player pressed d');
+                player.move_right();
+            }
+        });
+        //Deals with key up events
+        window.addEventListener('keyup', function(e) {
+            if (e.keyCode === 83) {
+                player.vx = 0;
+                player.vy = 0;
+            }
+            if (e.keyCode === 87) {}
+            if (e.keyCode === 65) {}
+            if (e.keyCode === 68) {}
+        });
+    },
+    //The following functions are called by the input handler to move the player left and right
+    move_left: function() {
+        this.vx = -this.speed;
+        this.vy = 0;
+        this.x += this.vx;
+        this.y += this.vy;
+    },
+    move_right: function() {
+        this.vx = this.speed;
+        this.vy = 0;
+        this.x += this.vx;
+        this.y += this.vy;
+    },
+    move_up: function() {
+        this.vx = 0;
+        this.vy = -this.speed;
+        this.x += this.vx;
+        this.y += this.vy;
+    },
+    move_down: function() {
+        this.vx = 0;
+        this.vy = this.speed;
+        this.x += this.vx;
+        this.y += this.vy;
+    },
+    //handles the player eating a square or something
+    eat: function() {}
 };
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBackground();
-  player.draw();
-  player.input();
-  player.colision();
-  window.requestAnimationFrame(draw);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+    player.draw();
+    player.input();
+    player.colision();
+    window.requestAnimationFrame(draw);
 }
 
 /* char codes for future refrence
--- scratch-pad.js
+++ scratch-pad.js
@@ -7,57 +7,57 @@ background.rect(0, 0, 640, 480);
 background.fill();
 
 //draw a square
-var drawSquare = function (size, posX, posY, color) {
-	var rect = ctx;
-	rect.fillStyle = color;
-	rect.fillRect(posX, posY, size, size);
-	return true;
+var drawSquare = function(size, posX, posY, color) {
+    var rect = ctx;
+    rect.fillStyle = color;
+    rect.fillRect(posX, posY, size, size);
+    return true;
 };
 
-var drawCheckerboard = function () {
-	var fourSq = function (posX, posY) {
-		// draw rect 1
-		var rect1 = ctx;
-		rect1.fillStyle = 'white';
-		rect1.fillRect(0 + posX, 0 + posY, 10, 10);
-		// draw rect 2
-		var rect2 = ctx;
-		rect2.fillStyle = 'black';
-		rect2.fillRect(0 + posX, 10 + posY, 10, 10);
-		// draw rect 4
-		var rect3 = ctx;
-		rect3.fillStyle = 'white';
-		rect3.fillRect(10 + posX, 10 + posY, 10, 10);
-		// draw rect 3
-		var rect4 = ctx;
-		rect4.fillStyle = 'black';
-		rect4.fillRect(10 + posX, 0 + posY, 10, 10);
-	};
-	//draw checker board
-	for (var y = 0; y <= 480; y = y + 20) {
-		for (var x = 0; x < 640; x = x + 20) {
-			fourSq(x, y);
-		}
-	}
+var drawCheckerboard = function() {
+    var fourSq = function(posX, posY) {
+        // draw rect 1
+        var rect1 = ctx;
+        rect1.fillStyle = 'white';
+        rect1.fillRect(0 + posX, 0 + posY, 10, 10);
+        // draw rect 2
+        var rect2 = ctx;
+        rect2.fillStyle = 'black';
+        rect2.fillRect(0 + posX, 10 + posY, 10, 10);
+        // draw rect 4
+        var rect3 = ctx;
+        rect3.fillStyle = 'white';
+        rect3.fillRect(10 + posX, 10 + posY, 10, 10);
+        // draw rect 3
+        var rect4 = ctx;
+        rect4.fillStyle = 'black';
+        rect4.fillRect(10 + posX, 0 + posY, 10, 10);
+    };
+    //draw checker board
+    for (var y = 0; y <= 480; y = y + 20) {
+        for (var x = 0; x < 640; x = x + 20) {
+            fourSq(x, y);
+        }
+    }
 };
 
 //moves a square across the screen
-var cat = function () {
-	for (var i = 0; i < 640; i++) {
-	//draws a square
-	drawSquare(10, i, 10, 'white')
-	//there should be a delay in here or something
-	
-	//clear the square
-	ctx.clearRect(i, 10, 10, 10);
-	//replace the black background
-	drawSquare(10,i,10, 'black');
-	return true;
-	}
+var cat = function() {
+    for (var i = 0; i < 640; i++) {
+        //draws a square
+        drawSquare(10, i, 10, 'white')
+            //there should be a delay in here or something
+
+        //clear the square
+        ctx.clearRect(i, 10, 10, 10);
+        //replace the black background
+        drawSquare(10, i, 10, 'black');
+        return true;
+    }
 };
 
-var help = function () {
-	console.log('this is a thingy about things');
-	console.log('fuckers are fuckers');
+var help = function() {
+    console.log('this is a thingy about things');
+    console.log('fuckers are fuckers');
 
 };
